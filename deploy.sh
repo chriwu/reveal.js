7524301bf66e720e6f7ef4c15ad9aa0b0e8afd98
chris werding <chriswerding@bueller.us>
2017-04-29 15:07:00 +0200

    * updated status badge * troubleshooting deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 echo Pushed to GCR
 
 #kubectl patch deployment frontend -p '{"spec":{"template":{"spec":{"containers":[{"name":"ps-tec-17","image":"eu.gcr.io/ps-teconomy-2017/ps-tec-17:'"$CIRCLE_SHA1"'"}]}}}}'
-kubectl set image deployment/demo demo=eu.gcr.io/${PROJECT_NAME}/${IMAGE_NAME}:$CIRCLE_SHA1
+kubectl set image deployment/demo demo=eu.gcr.io/ps-teconomy-2017/ps-tec-17:$CIRCLE_SHA1
