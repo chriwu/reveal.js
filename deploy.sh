ec500a5a337b9457d5d21eecbf28b737bb5d3244
chris werding <chriswerding@bueller.us>
2017-04-29 21:36:19 +0200

    * extended deploy script to update dyndns with public cluster ip address

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,7 @@ sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 echo Pushed to GCR
 
 kubectl set image deployment/demo demo=eu.gcr.io/${PROJECT_NAME}/${IMAGE_NAME}:$CIRCLE_SHA1
+
+SERVICE_IP=`kubectl get services | grep demo | awk -F ' ' '{print $3}'`
+echo "Kubernetes service IP: $SERVICE_IP"
+echo url="https://www.duckdns.org/update?domains=pstec&token=7b1e3eed-bc64-4edb-a370-52526ccbfba1&ip=$SERVICE_IP" | curl -k -K -
